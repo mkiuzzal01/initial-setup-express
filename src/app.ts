2811fbd7049f71da598fd690b4024778bbc0a2c7
MD Khairul Islam <mkiuzzal07@gmail.com>
2025-04-17 04:36:53 +0600

    intial setup of express application

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,19 @@
 import express, { Request, Response } from 'express';
 import cors from 'cors';
-import router from './app/router';
+// import router from './app/router';
 
 const app = express();
 app.use(express.json());
 app.use(cors({ origin: ['localhost:5173'] }));
 app.use(express.text());
 
-app.get('/', (req, res) => {
+// Home route:
+app.get('/', (req: Request, res: Response) => {
   res.send('Welcome to Health Track');
 });
 
 // All application routes:
-app.use('/api/v1/', router);
+// app.use('/api/v1/', router);
 
 // This is  just test:
 app.get('/', (req: Request, res: Response) => {
--- src/app/router/index.ts
+++ src/app/router/index.ts
@@ -1,13 +1,13 @@
-import { Router } from 'express';
-const router = Router();
+// import { Router } from 'express';
+// const router = Router();
 
-const routeModule = [
-  {
-    path: '',
-    route: '',
-  },
-];
+// const routeModule = [
+//   {
+//     path: '',
+//     route: null,
+//   },
+// ];
 
-routeModule.forEach((route) => router.use(route.path, route.route));
+// routeModule.forEach((route) => router.use(route.path, route?.route));
 
-export default router;
+// export default router;
